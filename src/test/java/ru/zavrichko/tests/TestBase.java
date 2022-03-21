498cc4571315a1931ab8b2b74b759f4148a0a785
Andrey Zavrichko <andrey@zavrichko.dev>
2022-03-21 18:53:59 +0200

    create project

--- src/test/java/ru/zavrichko/tests/TestBase.java
+++ src/test/java/ru/zavrichko/tests/TestBase.java
@@ -15,8 +15,8 @@ public class TestBase {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-       // Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        Configuration.remote = System.getProperty("remote_driver_url", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+       // Configuration.remote = System.getProperty("remote_driver_url", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
        // Configuration.remote = System.getProperty("remote_driver_url", "http://149.154.70.38:4444/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
